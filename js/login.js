e3399ce3d115a171e54f95ce97b3944d0048cc62
gianina.28 <gianina.libonatti28@gmail.com>
2021-09-05 15:42:08 -0300

    guardar usuario

--- js/login.js
+++ js/login.js
@@ -1,7 +1,49 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+
+let btn = document.getElementById("btn");
+	if (btn) {
+		btn.addEventListener("click", function (e) {
+			validarFormulario (e);
+		}); 
+	}
+	let usr = localStorage.getItem("Usuario");
+	if (usr) {
+		let usrLink = document.getElementById("usuario.pag");
+		if (usrLink) {usrLink.innerHTML = "Usuario : "+ usr;}
+	}
+    document.addEventListener ("DOMContentLoaded", function(e){
+        document.getElementById("btn").addEventListener("click", function (e) {
+            validarFormulario (e);
+        }); 
+    });
 
 
-});
+function validarFormulario (evento) {
+	evento.preventDefault();
+	let user= document.getElementById ("Usuario").value
+	let contraseña = document.getElementById ("Contraseña").value;
+	if (user.length == 0 ) {
+		alert ("¡Vaya! Parece que te has olvidado de colocar tu mail")
+        return;
+	}
+    
+	if (contraseña.length == 0) {
+		alert ("¡Vaya! Parece que te has olvidado de colocar tu contraseña")
+        return;
+	}
+	else  {
+		let fin = user.indexOf("@");
+		let Usuario = user.substring (0, fin);
+		localStorage.setItem ("Usuario",Usuario);
+		console.log(Usuario);
+        var usuario = localStorage.getItem("Usuario");
+		window.location.href = "./home.html";
+    
+	}
+    document.addEventListener("DOMContentLoaded", function (e) {
+        document.getElementById("usuario.pag").innerHTML =localstorage.getItem("Usuario");
+    })
+    
+};
